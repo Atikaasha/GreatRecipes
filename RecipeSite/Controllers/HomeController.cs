9c9523480284a7ea40e5f865821acad625ef6ff6
Chowdhury Atika Parvin <atikachowdhury2@gmail.com>
2019-10-04 11:55:56 -0400

    MVC created

---
+++ RecipeSite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace RecipeSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        //default action method
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("Home");
+        }
+        [HttpGet]
+        public ViewResult RecipeList()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult ViewRecipe()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult ReviewRecipe()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult AddRecipe()
+        {
+            return View();
+        }
+    }
+}
---
+++ RecipeSite/Models/ViewerResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeSite.Models
+{
+    public class ViewerResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public String Name { get; set; }
+        [Required(ErrorMessage = "Please enter your Email")]
+        public String Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone")]
+        public String Phone { get; set; }
+        [Required(ErrorMessage = "Please let us know your opinion")]
+        public bool? DoYouLike { get; set; }
+    }
+}
